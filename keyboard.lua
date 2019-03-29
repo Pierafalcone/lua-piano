e71eb46dd5365787a19e3e9e332b003ff3a7cc54
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-03-29 16:06:07 +0100

    Added key press check

--- keyboard.lua
+++ keyboard.lua
@@ -1,24 +1,27 @@
 local keyboard = {}
 
 function keyboard.draw(dt)
-    -- key options
-    local key = {}
-    key.width = 50
-    key.height = 160
-    key.offset = 10
+    -- white_key options
+    local white_key = {}
+    white_key.width = 50
+    white_key.height = 160
+    white_key.offset = 10
     -- board options
     local board = {}
     board.start_x = 30
     board.start_y = 600
-    -- draw keys
+    -- draw white keys
     for i = 1, 7 do
         love.graphics.rectangle(
             "fill", 
-            board.start_x + (key.width + key.offset) * i, 
+            board.start_x + (white_key.width + white_key.offset) * i, 
             board.start_y, 
-            key.width, 
-            key.height)
+            white_key.width, 
+            white_key.height)
     end
+
+    -- PIERA: now you can draw black keys down here :)
+    -- if you cycle inside key_pressed you can check if a key is pressed or not
 end
 
 return keyboard
--- main.lua
+++ main.lua
@@ -1,16 +1,18 @@
+
 local mario = require("mario")
 local keyboard = require("keyboard")
 
 keys = {}
+keys.a = "do"
+keys.s = "re"
+keys.d = "mi"
+keys.f = "fa"        
+keys.g = "sol"
+keys.h = "la"
+keys.j = "si"
+keys.k = "doa"
 
-keys["a"] = "do"
-keys["s"] = "re"
-keys["d"] = "mi"
-keys["f"] = "fa"        
-keys["g"] = "sol"
-keys["h"] = "la"
-keys["j"] = "si"
-keys["k"] = "doa"
+key_pressed = {}
 
 color = { 0, 0, 0 }
 
@@ -22,34 +24,44 @@ success = love.window.setMode(800, 800, flags)
 
 function love.load()
     resources.mario = love.graphics.newImage("mario.png")
+    love.keyboard.setKeyRepeat(false)
+    -- set default key state to false
+    for key, value in pairs(keys) do
+        key_pressed[key] = false
+    end
 end
 
 function love.update(dt)
    mario.update(dt)
 end
 
-function play(note)
+function play_key(note)
     source = love.audio.newSource(note..".mp3", "static")
     love.audio.play(source)
     --love.graphics.draw(resources.mario, math.random(0, 500), math.random(0, 500))
 end
 
-function love.draw(dt)
-    love.graphics.setBackgroundColor(color)
+function love.update(dt)
     for key, note in pairs(keys) do
-        if love.keyboard.isDown(key) then
-            love.graphics.print(note, 100, 100)
-            --love.graphics.rectangle("fill", 30, 200, 60, 120)
-            play(note)     
+        if key_pressed[key] == true then
+            play_key(note)    
+            -- this avoids the keys repetition
+            key_pressed[key] = false 
         end
     end
+end
+
+function love.draw(dt)
+    love.graphics.setBackgroundColor(color)
     mario.draw()
     keyboard.draw()
 end
 
-function love.keypressed()
+function love.keypressed(key)
     color = { math.random(255) / 255, math.random(255) / 255, math.random(255) / 255 }
+    key_pressed[key] = true
 end
 
 function love.keyreleased(key)
+    key_pressed[key] = false
  end
